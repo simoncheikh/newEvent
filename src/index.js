c768cf4d3ed806db9fd07d8bbeecb3f0e5ed7efd
simoncheikh <simoncheikh710@gmail.com>
2024-03-01 09:02:38 +0200

    new

--- src/index.js
+++ src/index.js
@@ -27,25 +27,25 @@ ReactDOM.render(
     <AuthProvider>
       <React.StrictMode>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/event" element={<EventPage />} />
-          <Route path="/SignIn" element={<SignIn />} />
-          <Route path="/EventInfo" element={<EventInfo />} />
-          <Route path="/AddEvent" element={<AddEvent />} />
-          <Route path="/Cart" element={<Cart />} />
-          <Route path="/CheckOut" element={<CheckOut />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/Privacy" element={<PrivacyAndCondition />} />
-          <Route path="/Contact" element={<Contact />} />
+          <Route path="/newEvent" element={<HomePage />} />
+          <Route path="/newEvent/event" element={<EventPage />} />
+          <Route path="/newEvent/SignIn" element={<SignIn />} />
+          <Route path="/newEvent/EventInfo" element={<EventInfo />} />
+          <Route path="/newEvent/AddEvent" element={<AddEvent />} />
+          <Route path="/newEvent/Cart" element={<Cart />} />
+          <Route path="/newEvent/CheckOut" element={<CheckOut />} />
+          <Route path="/newEvent/Profile" element={<Profile />} />
+          <Route path="/newEvent/Privacy" element={<PrivacyAndCondition />} />
+          <Route path="/newEvent/Contact" element={<Contact />} />
           {/* admin */}
-          <Route path="/admin/Home" element={<HomeAdmin />} />
-          <Route path="/admin/users" element={<Users />} />
-          <Route path="/admin/events" element={<Events />} />
-          <Route path="/admin/methodPayment" element={<MethodPayment />} />
-          <Route path="/admin/Order" element={<Order />} />
-          <Route path="/admin/Receipt" element={<Receipt />} />
+          <Route path="/newEvent/admin/Home" element={<HomeAdmin />} />
+          <Route path="/newEvent/admin/users" element={<Users />} />
+          <Route path="/newEvent/admin/events" element={<Events />} />
+          <Route path="/newEvent/admin/methodPayment" element={<MethodPayment />} />
+          <Route path="/newEvent/admin/Order" element={<Order />} />
+          <Route path="/newEvent/admin/Receipt" element={<Receipt />} />
           {/*Client*/}
-          <Route path="/client/Home" element={<HomeClient />} />
+          <Route path="/newEvent/client/Home" element={<HomeClient />} />
         </Routes>
       </React.StrictMode>
     </AuthProvider>
